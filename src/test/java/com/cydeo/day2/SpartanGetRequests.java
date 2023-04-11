641ce481a8694b49b193880de6c7942783a1333d
Mustafa <mustafamuezzinoglu@gmail.com>
2023-04-12 02:43:44 +0300

    second test case done

--- src/test/java/com/cydeo/day2/SpartanGetRequests.java
+++ src/test/java/com/cydeo/day2/SpartanGetRequests.java
@@ -4,6 +4,7 @@ import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 import io.restassured.response.Response;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class SpartanGetRequests {
@@ -17,7 +18,7 @@ public class SpartanGetRequests {
 //    And response body should include spartan result
 
     @Test
-    public void test1(){
+    public void test1() {
 
         Response response = RestAssured.given().accept(ContentType.JSON)
                 .when()
@@ -34,5 +35,32 @@ public class SpartanGetRequests {
 
 //        how to do APi testing then ?
         Assertions.assertEquals(response.contentType(), "application/json");
+    }
+
+         /*
+        Given accept header is application/json
+        When users sends a get request to /api/spartans/3
+        Then status code should be 200
+        And content type should be application/json
+        and json body should contain Fidole
+     */
+
+    @DisplayName("GET one spartan/api/spartans/3 and verify")
+    @Test
+    public void test2() {
+
+        Response response = RestAssured.given().accept(ContentType.JSON)
+                .when()
+                .get(baseUrl + "/api/spartans/3");
+
+//      verify status code 200
+        Assertions.assertEquals(response.statusCode(), 200);
+
+//        verify content type
+        Assertions.assertEquals(response.contentType(), "application/json");
+
+//        verify json body contains Fidole
+        Assertions.assertTrue(response.body().asString().contains("Fidole"));
+
     }
 }
